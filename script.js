027063966b8cfccc08d783fd3992be326dc5ec0f
Mihir Shinde <mihir2403@gmail.com>
2023-01-29 15:55:58 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+//cart
+
+const bar = document.getElementById("bar");
+const close = document.getElementById("close");
+const navbar = document.getElementById("navbar");
+
+
+//Open cart
+bar.onclick = ()=>{
+  navbar.classList.add("active");
+}
+
+//Close cart
+close.onclick = ()=>{
+  navbar.classList.remove("active");
+}
